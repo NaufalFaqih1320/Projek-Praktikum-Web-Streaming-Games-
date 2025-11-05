2bb26fb2a925108fc7ff7bd11b0a4f275f4f7868
Albert <albertusbagus.dya@gmail.com>
2025-11-05 20:29:12 +0700

    membuat login dan register

---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+
+        body{
+            font-family: 'Poppins';
+        }
+
+        .card-container{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #121315;
+            flex-direction: column;
+            color: #FFFFFF;
+        }
+
+        .card{
+            border-radius: 15px;
+            background-color: #1E1E1E;
+            color: #FFFFFF;
+        }
+
+
+    </style>
+</head>
+<body>
+    <div class="card-container ">
+        <h1>Halo, Gamers!</h1>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="asset/image 8.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
