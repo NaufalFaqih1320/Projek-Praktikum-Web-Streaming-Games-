4a72e92c013f2433cae30e4e14863b1615c82939
Albert <albertusbagus.dya@gmail.com>
2025-11-25 17:27:11 +0700

    nambah navbar.php

--- index.php
+++ index.php
@@ -126,34 +126,7 @@ if (!isset($_SESSION['login'])) {
 </head>
 <body>
     <!-- Navbar Start -->
-    <nav>
-        <div class="navbar">
-            <div class="navbar-logo">
-                <a href="#"><img src="asset/Group 20.png" alt="" width="80px"></a>
-            </div>
-            <div class="navbar-search">
-                <div class="search-toggle"><i data-feather="search" style="color: #2F3032;margin: 0 5px 0 15px"></i></div>
-                <div class="search-placeholder"><input type="text" placeholder="Search" class="input-search"></div>
-                <div class="search-button"><button type="submit"><span>Search</span></button></div>
-            </div>
-            <div class="navbar-stream">
-                <div class="stream"><i data-feather="video" class="logo-stream"></i></div><span>Start Stream</span>
-            </div>
-            <div class="navbar-setting">
-                <i data-feather="settings" style="color: #9F9F9F; transition: 0.3s" class="logo-setting"></i>
-            </div>
-            <div class="navbar-notif">
-                <i data-feather="bell" style="color: #9F9F9F; transition: 0.3s" class="logo-notif"></i>   
-            </div>
-            <div class="navbar-profile">
-                <div class="profile-picture"><img src="asset/Mask group.png" alt=""></div>
-                <div class="profile-info">
-                    <span class="profile-name">Albertus</span>
-                    <span class="profile-status">Online</span>
-                </div>
-            </div>
-        </div>
-    </nav> 
+    <?php include "navbar.php"; ?>
     <!-- Navbar End -->
 
     <!-- Toast Login Berhasil -->
